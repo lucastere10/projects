4b1aa999bc83ef5e6a07dfae55ad09f637b8bfa7
Lucas Caldas <lucasmedeiroscaldas@yahoo.com.br>
2022-08-15 16:02:05 -0300

    add tree star

--- Python Projects/tree_test.py
+++ Python Projects/tree_test.py
@@ -8,26 +8,31 @@ base_string = '|'
 star_string = "*"
 
 #input de valores
+tree_string = input('Qual string utilizar na sua árvore:')
 tree_input =  round(float(input("Quantos andares tem sua árvore: ")))
 base_input = round(float(input("Qual o tamanho do caule da sua árvore: ")))
+star_input = bool(input('Deseja adicionar uma estrela no topo da sua árvore? (Deixar vazio caso não)'))
+
 tree_list = range(1, tree_input + 1, 1)
 base_list = range(1, base_input + 1, 1)
 
 #valores iniciais
-space = tree_input - 1  #espaço árvore
-tree_leaf = 1 #somatório por andar da árvore
-base = tree_input - 2 #tamanho do caule
+n_space = tree_input - 1  #espaço árvore
+n_leaf = 1 #somatório por andar da árvore
+n_base = tree_input - 2 #tamanho do caule
 
 #estrela no top da árvore
-print(space_string * space +  star_string * tree_leaf)
+if star_input == True:
+    print(space_string * n_space +  star_string * n_leaf)
 
 #printar folhas da árvore
 for n in tree_list:
-    print(space_string * space +  tree_string * tree_leaf)
+    print(space_string * n_space +  tree_string * n_leaf)
     #cálculos
-    space = space - 1
-    tree_leaf = tree_leaf + 2
+    n_space = n_space - 1
+    n_leaf = n_leaf + 2
 
 #print base
-for n in base_list:
-    print((base * space_string) + base_string * 3)
+if tree_input >= 3:   
+    for n in base_list:
+        print((n_base * space_string) + base_string * 3)
