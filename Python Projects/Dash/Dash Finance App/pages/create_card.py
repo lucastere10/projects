5bfc0a1148d2957f51ff288e5b34969093410f5c
Lucas Caldas <lucasmedeiroscaldas@yahoo.com.br>
2022-10-05 20:10:45 -0300

    Create card now pull values from yfinance automatically

--- Python Projects/Dash/Dash Finance App/pages/create_card.py
+++ Python Projects/Dash/Dash Finance App/pages/create_card.py
@@ -1,11 +1,30 @@
 from dash import dcc, html, State, Input, Output
 import dash_bootstrap_components as dbc    # pip install dash-bootstrap-components
 import yfinance as yf
+import plotly.graph_objects as go
+from sidebar import nasdaq
 
 # card ---------------
 def create_card(ticker):  
     df = yf.Ticker(ticker)
     info = df.info
+    hist = df.history('1d','1m').reset_index()
+    while bool(hist.empty) is True:
+        print('deu ruim')
+        df = yf.Ticker(nasdaq['Symbol'].sample().to_string().split()[1])
+        hist = df.history('1d','1m').reset_index()
+        if bool(hist.empty) is False:
+            print('agora deu bom')
+            break
+    fig = go.Figure(data=[go.Candlestick(x=hist['Datetime'],
+                    open  = hist['Open'],
+                    high  = hist['High'],
+                    low   = hist['Low'],
+                    close = hist['Close'])
+                    ])
+    fig.update_layout(xaxis_rangeslider_visible=False,
+                      yaxis={'visible': False, 'showticklabels': False},
+                      xaxis={'visible': False, 'showticklabels': False},)
     return dbc.Card([
             dbc.Row([
                 dbc.Col([
@@ -15,42 +34,47 @@ def create_card(ticker):
                         style = {"width":'4rem', "padding": "0.5rem 0.5rem"},
                         class_name = "rounded-circle",
                     ),
-                ], width = 2),
+                ], width = 3),
                 dbc.Col([
                     dbc.Row([html.H4(info['shortName'], id = f"{ticker}-title")], style={'font-size': '6px'}),
                     dbc.Row([html.P(info['symbol'], id = f'{ticker}-id')], style={'font-size': '16px'}),
-                ], width = 10),
+                ], width = 9),
             ]),
             dbc.CardBody([
                 dbc.Row([               #GRAPH
                     dbc.Col([
-                        dcc.Graph(figure={},
-                                    config={'displayModeBar':False})
+                        dcc.Graph(figure=fig)
                     ])
                 ]),
                 dbc.Row([               # CHANGE, SELL, BUY
                     dbc.Col([
-                        dbc.Button("CHANGE"),
-                    ], width = 4),
+                        html.H5("OPEN"),
+                    ], width = 3),
                     dbc.Col([
-                        dbc.Button("LOW"),
-                    ], width = 4),
+                        html.H5("HIGH"),
+                    ], width = 3),
                     dbc.Col([
-                        dbc.Button("HIGH"),
-                    ], width = 4)
+                        html.H5("LOW"),
+                    ], width = 3),
+                    dbc.Col([
+                        html.H5("CLOSE"),
+                    ], width = 3),
                 ], align = 'center', justify="evenly"
                 ),
                 dbc.Row([
                     dbc.Col([
-                        dbc.Label(children="1,32%")
-                    ], width = 4),
+                        dbc.Label("${:,.2f}".format(float(hist['Open'][-1:])))
+                    ], width = 3),
                     dbc.Col([
-                        dbc.Label(children="12.237")
-                    ], width = 4),
+                        dbc.Label("${:,.2f}".format(float(hist['High'][-1:])))
+                    ], width = 3),
                     dbc.Col([
-                        dbc.Label(children="13.418")
-                    ], width = 4)
-                ],align = 'center', justify="evenly"
+                        dbc.Label("${:,.2f}".format(float(hist['Low'][-1:])))
+                    ], width = 3),
+                    dbc.Col([
+                        dbc.Label("${:,.2f}".format(float(hist['Close'][-1:])))
+                    ], width = 3),
+                ], align = 'center', justify="evenly"
                 ),
             ])
         ], style={"width": "22rem"}
--- Python Projects/Dash/Dash Finance App/pages/home.py
+++ Python Projects/Dash/Dash Finance App/pages/home.py
@@ -5,46 +5,68 @@ import dash  #pip install dash
 from dash import html, dcc, Output, Input, State, dash_table                    
 import dash_bootstrap_components as dbc    # pip install dash-bootstrap-components
 import dash_daq as daq                     # pip install dash_daq
+from dash.exceptions import PreventUpdate
 import plotly.express as px
 import plotly.graph_objects as go
 from create_card import create_card
-from sidebar import sidebar, search_bar
+from sidebar import sidebar, search_bar, nasdaq, callback_sidebar_collapse, callback_open_offcanvas, callback_filter_dropdown
+from styles.styles import SIDEBAR_STYLE, SIDEBAR_HIDEN, CONTENT_STYLE, CONTENT_STYLE1
 
 # APP --------------
 app = dash.Dash(__name__, external_stylesheets= [dbc.themes.LUX, dbc.icons.FONT_AWESOME],
+                suppress_callback_exceptions=True,
                 meta_tags=[{'name': 'viewport',
                             'content': 'width=device-width, initial-scale=1.0'}]
                 )
 
-### STYLES ### ------------------------------------------------------------------
-# Content Style ------------
-CONTENT_STYLE = {
-    "margin-left": "15rem",
-    "margin-right": "2rem",
-    "padding": "2rem 1rem",
-    "background-color": "#f8f9fa",
-}
-CONTENT_STYLE1 = {
-    "transition": "margin-left .5s",
-    "margin-left": "2rem",
-    "margin-right": "2rem",
-    "padding": "2rem 1rem",
-    "background-color": "#f8f9fa",
-}
-
+# LAYOUT --------------
 app.layout = dbc.Container([
     #chart grid
+    dcc.Interval(
+        id='interval-component-id',
+        interval=1*300000, # in milliseconds
+        n_intervals=0
+    ),
     dbc.Row(dbc.Col([search_bar], width = 10, align='center'), justify="center"),
     html.Br(),
+    #dbc.Row(dbc.Col(dbc.Button('GET DATA',id = 'chart-button-id'), width = 2)),
+    #html.Br(),
     dbc.Row([
-        dbc.Col([html.Div(create_card('PBR'))]),
-        dbc.Col([html.Div(create_card('AAPL'))]),
-        dbc.Col([html.Div(create_card('AMZN'))]),
+        dbc.Col(id='chart1-id'),
+        dbc.Col(id='chart2-id'),
+        dbc.Col(id='chart3-id'),
     ]),
     html.Br(),
+    dbc.Row([
+        dcc.Loading(
+            id="loading-id", type="default", color = '#1a1a1a',
+            children=html.Div(id="loading-output-id"),
+        ),
+    ]),
     sidebar,
-], style = CONTENT_STYLE)
+], style = CONTENT_STYLE, id = 'layout-id')
 
+### CALLBACKS ### ------------------------------------------------------------------
+
+#load charts
+@app.callback(
+    Output('loading-output-id','children'),
+    Output('chart1-id','children'),    
+    Output('chart2-id','children'),
+    Output('chart3-id','children'),
+    Input('interval-component-id', 'n_intervals')   
+)
+def get_cards(x):
+    print(x)
+    return ['Loading Data',
+            create_card(nasdaq['Symbol'].sample().to_string().split()[1]), 
+            create_card(nasdaq['Symbol'].sample().to_string().split()[1]), 
+            create_card(nasdaq['Symbol'].sample().to_string().split()[1])]
+            #nasdaq['Symbol'].sample().to_string().split()[1]
+        
+
+#collapse sidebar
+callback_sidebar_collapse(app)
 
 if __name__=='__main__':
     app.run_server(debug=True, port=3000)
