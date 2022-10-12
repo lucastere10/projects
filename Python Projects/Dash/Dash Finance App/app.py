da3d5c09c52b368c19693a69c3e90b58be5f88c4
Lucas Caldas <lucasmedeiroscaldas@yahoo.com.br>
2022-10-12 18:49:46 -0300

    Creating Multipage Structure

--- Python Projects/Dash/Dash Finance App/app.py
+++ Python Projects/Dash/Dash Finance App/app.py
@@ -1,15 +1,15 @@
 #---------------------------------------------
 ### LIBS ###
-from contextlib import nullcontext
 from turtle import ht, width
 import dash  #pip install dash
-from dash import html, dcc, Output, Input, State, dash_table, callback                    
+from dash import html, dcc, Output, Input, State, dash_table                    
 import dash_bootstrap_components as dbc    # pip install dash-bootstrap-components
 import dash_daq as daq                     # pip install dash_daq
 from dash.exceptions import PreventUpdate
-import pandas as pd 
-import requests
-from pages import contact, home, overview, not_found_404
+import plotly.express as px
+import plotly.graph_objects as go
+from pages.sidebar import sidebar, search_bar
+from pages.styles import styles #SIDEBAR_STYLE, SIDEBAR_HIDEN, CONTENT_STYLE, CONTENT_STYLE1
 
 app = dash.Dash(__name__, use_pages=True, 
                 external_stylesheets= [dbc.themes.LUX, dbc.icons.FONT_AWESOME],
@@ -19,18 +19,11 @@ app = dash.Dash(__name__, use_pages=True,
                 )
 server = app.server
 
-app.layout = html.Div([
-    dcc.Location(id='url', refresh=False),
-    html.Div(id='page-content')
-])
-
-@callback(Output('page-content', 'children'),
-              Input('url', 'pathname'))
-def display_page(pathname):
-    if pathname == '/':
-        return contact.layout
-    else:
-        return not_found_404.layout
+app.layout = dbc.Container([
+    html.Div([dbc.Row(dbc.Col([search_bar], width = 10, align='center'), justify="center")]),
+    html.Div(id='content-id'),
+    html.Div([sidebar.sidebar]),
+], style = styles.CONTENT_STYLE, id = 'layout-id')
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- Python Projects/Dash/Dash Finance App/pages/contact.py
+++ Python Projects/Dash/Dash Finance App/pages/contact.py
@@ -3,13 +3,11 @@ from dash import html, dcc, Output, Input, State
 import dash_bootstrap_components as dbc    # pip install dash-bootstrap-components
 import dash_daq as daq                     # pip install dash_daq
 
-dash.register_page(__name__)
+dash.register_page(__name__, path = '/')
 
 layout = html.Div(children=[
     html.H1(children='This is our Contact page'),
-
     html.Div(children='''
         This is our Contact page content.
     '''),
-
 ])
