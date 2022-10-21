5a3ef8a530263936e1fb9e8e1b5cbcc0b8ee385f
Lucas Caldas <lucasmedeiroscaldas@yahoo.com.br>
2022-10-21 17:04:07 -0300

    New Project Structure

--- Python Projects/Dash/Dash Rent Prediction App/rent_app.py
+++ Python Projects/Dash/Dash Rent Prediction App/rent_app.py
@@ -5,7 +5,7 @@ import dash
 import dash_bootstrap_components as dbc
 from dash import dcc, html
 from dash.dependencies import Input, Output
-from assets import eda_dashboard, ml_analysis, dataframe
+from modules import eda_dashboard, ml_analysis, module_dataframe
 
 ### - Styles - ####
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -14,16 +14,12 @@ server = app.server
 colors = {"background": "#F3F6FA", "background_div": "white", 'text': '#009999'}
 app.config['suppress_callback_exceptions']= True
 
-#load dataframe
-#return  dataframe.df
-
 ### - LAYOUT - ###
 app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
     html.H1('House Rent Prediction Dashboard', style={
             'textAlign': 'center',
             'color': colors['text']
         }),
-
       dcc.Tabs(id="tabs", className="row", style={"margin": "2% 3%","height":"20","verticalAlign":"middle"}, value='dem_tab', children=[
         dcc.Tab(label='EDA Dashboard', value='eda_tab'),
         dcc.Tab(label='ML Analysis', value='ml_tab')
